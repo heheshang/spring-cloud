09a548e15a0d3a6b0c3a279091ee23d035c6ab5b
heheshang <2448318433@qq.com>
2017-06-26 11:31:27 +0800

    uu

--- apache-ignite/spring-cloud-ignite-demo - 1/src/main/java/com/example/ignite/conf/IgniteConfig.java
+++ apache-ignite/spring-cloud-ignite-demo - 1/src/main/java/com/example/ignite/conf/IgniteConfig.java
@@ -2,8 +2,14 @@ package com.example.ignite.conf;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/20 0020.
@@ -13,6 +19,20 @@ public class IgniteConfig {
 
 	@Bean
 	public Ignite ignite(){
-		return Ignition.start();
+		IgniteConfiguration cfg = new IgniteConfiguration();
+		cfg.setPeerClassLoadingEnabled(true);
+		cfg.setDiscoverySpi(tcpDiscoverySpi());
+		return Ignition.start(cfg);
+	}
+
+	@Bean
+	TcpDiscoverySpi tcpDiscoverySpi(){
+		TcpDiscoverySpi spi = new TcpDiscoverySpi();
+		TcpDiscoveryVmIpFinder ipFinder = new  TcpDiscoveryVmIpFinder();
+		List<String> address = new ArrayList<>();
+		address.add("192.168.61.129:47500");
+		ipFinder.setAddresses(address);
+		spi.setIpFinder(ipFinder);
+		return spi;
 	}
 }
