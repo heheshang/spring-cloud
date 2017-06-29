8793205471e558fc07f3b5d49bd75dc7a3d9c96a
heheshang <2448318433@qq.com>
2017-06-29 17:09:48 +0800

    innite demo

---
+++ apache-ignite/examples/src/test/java/org/apache/ignite/examples/CacheExamplesSelfTest.java
@@ -0,0 +1,193 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.examples;
+
+import org.apache.ignite.examples.datagrid.CacheAffinityExample;
+import org.apache.ignite.examples.datagrid.CacheEntryProcessorExample;
+import org.apache.ignite.examples.datagrid.CacheApiExample;
+import org.apache.ignite.examples.datagrid.CacheContinuousQueryExample;
+import org.apache.ignite.examples.datagrid.CacheDataStreamerExample;
+import org.apache.ignite.examples.datagrid.CachePutGetExample;
+import org.apache.ignite.examples.datagrid.CacheQueryDmlExample;
+import org.apache.ignite.examples.datagrid.CacheQueryExample;
+import org.apache.ignite.examples.datagrid.CacheTransactionExample;
+import org.apache.ignite.examples.datagrid.MemoryPoliciesExample;
+import org.apache.ignite.examples.datagrid.starschema.CacheStarSchemaExample;
+import org.apache.ignite.examples.datagrid.store.CacheLoadOnlyStoreExample;
+import org.apache.ignite.examples.datastructures.IgniteAtomicLongExample;
+import org.apache.ignite.examples.datastructures.IgniteAtomicReferenceExample;
+import org.apache.ignite.examples.datastructures.IgniteAtomicSequenceExample;
+import org.apache.ignite.examples.datastructures.IgniteAtomicStampedExample;
+import org.apache.ignite.examples.datastructures.IgniteCountDownLatchExample;
+import org.apache.ignite.examples.datastructures.IgniteQueueExample;
+import org.apache.ignite.examples.datastructures.IgniteLockExample;
+import org.apache.ignite.examples.datastructures.IgniteSemaphoreExample;
+import org.apache.ignite.examples.datastructures.IgniteSetExample;
+import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
+
+/**
+ * Cache examples self test.
+ */
+public class CacheExamplesSelfTest extends GridAbstractExamplesTest {
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheAffinityExample() throws Exception {
+        CacheAffinityExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheEntryProcessorExample() throws Exception {
+        CacheEntryProcessorExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheAtomicLongExample() throws Exception {
+        IgniteAtomicLongExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheAtomicReferenceExample() throws Exception {
+        IgniteAtomicReferenceExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheAtomicSequenceExample() throws Exception {
+        IgniteAtomicSequenceExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheAtomicStampedExample() throws Exception {
+        IgniteAtomicStampedExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheCountDownLatchExample() throws Exception {
+        IgniteCountDownLatchExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheSemaphoreExample() throws Exception {
+        IgniteSemaphoreExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheLockExample() throws Exception {
+        IgniteLockExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheQueueExample() throws Exception {
+        IgniteQueueExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheSetExample() throws Exception {
+        IgniteSetExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheQueryExample() throws Exception {
+        CacheQueryExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheQueryDmlExample() throws Exception {
+        CacheQueryDmlExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheApiExample() throws Exception {
+        CacheApiExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheTransactionExample() throws Exception {
+        CacheTransactionExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheDataStreamerExample() throws Exception {
+        CacheDataStreamerExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCachePutGetExample() throws Exception {
+        CachePutGetExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testSnowflakeSchemaExample() throws Exception {
+        CacheStarSchemaExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheContinuousQueryExample() throws Exception {
+        CacheContinuousQueryExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testCacheLoadOnlyStoreExample() throws Exception {
+        CacheLoadOnlyStoreExample.main(EMPTY_ARGS);
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testMemoryPolicyExample() throws Exception {
+        MemoryPoliciesExample.main(EMPTY_ARGS);
+    }
+}
---
+++ apache-ignite/examples/src/test/java/org/apache/ignite/testsuites/IgniteExamplesSelfTestSuite.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.testsuites;
+
+import junit.framework.TestSuite;
+import org.apache.ignite.examples.BasicExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.BasicExamplesSelfTest;
+import org.apache.ignite.examples.CacheClientBinaryExampleTest;
+import org.apache.ignite.examples.CacheExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.CacheExamplesSelfTest;
+import org.apache.ignite.examples.CheckpointExamplesSelfTest;
+import org.apache.ignite.examples.ClusterGroupExampleSelfTest;
+import org.apache.ignite.examples.ComputeClientBinaryExampleTest;
+import org.apache.ignite.examples.ContinuationExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.ContinuationExamplesSelfTest;
+import org.apache.ignite.examples.ContinuousMapperExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.ContinuousMapperExamplesSelfTest;
+import org.apache.ignite.examples.DeploymentExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.DeploymentExamplesSelfTest;
+import org.apache.ignite.examples.EventsExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.EventsExamplesSelfTest;
+import org.apache.ignite.examples.IgfsExamplesSelfTest;
+import org.apache.ignite.examples.LifecycleExamplesSelfTest;
+import org.apache.ignite.examples.MemcacheRestExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.MemcacheRestExamplesSelfTest;
+import org.apache.ignite.examples.MessagingExamplesSelfTest;
+import org.apache.ignite.examples.MonteCarloExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.MonteCarloExamplesSelfTest;
+import org.apache.ignite.examples.SpringBeanExamplesSelfTest;
+import org.apache.ignite.examples.SpringDataExampleSelfTest;
+import org.apache.ignite.examples.TaskExamplesMultiNodeSelfTest;
+import org.apache.ignite.examples.TaskExamplesSelfTest;
+
+/**
+ * Examples test suite.
+ * <p>
+ * Contains only Spring ignite examples tests.
+ */
+public class IgniteExamplesSelfTestSuite extends TestSuite {
+    /**
+     * @return Suite.
+     * @throws Exception If failed.
+     */
+    public static TestSuite suite() throws Exception {
+//        System.setProperty(IGNITE_OVERRIDE_MCAST_GRP,
+//            GridTestUtils.getNextMulticastGroup(IgniteExamplesSelfTestSuite.class));
+
+        TestSuite suite = new TestSuite("Ignite Examples Test Suite");
+
+        suite.addTest(new TestSuite(CacheExamplesSelfTest.class));
+        suite.addTest(new TestSuite(BasicExamplesSelfTest.class));
+        suite.addTest(new TestSuite(ContinuationExamplesSelfTest.class));
+        suite.addTest(new TestSuite(ContinuousMapperExamplesSelfTest.class));
+        suite.addTest(new TestSuite(DeploymentExamplesSelfTest.class));
+        suite.addTest(new TestSuite(EventsExamplesSelfTest.class));
+        suite.addTest(new TestSuite(LifecycleExamplesSelfTest.class));
+        suite.addTest(new TestSuite(MessagingExamplesSelfTest.class));
+        suite.addTest(new TestSuite(MemcacheRestExamplesSelfTest.class));
+        suite.addTest(new TestSuite(MonteCarloExamplesSelfTest.class));
+        suite.addTest(new TestSuite(TaskExamplesSelfTest.class));
+        suite.addTest(new TestSuite(SpringBeanExamplesSelfTest.class));
+        suite.addTest(new TestSuite(SpringDataExampleSelfTest.class));
+        suite.addTest(new TestSuite(IgfsExamplesSelfTest.class));
+        suite.addTest(new TestSuite(CheckpointExamplesSelfTest.class));
+        suite.addTest(new TestSuite(ClusterGroupExampleSelfTest.class));
+
+        // Multi-node.
+        suite.addTest(new TestSuite(CacheExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(BasicExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(ContinuationExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(ContinuousMapperExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(DeploymentExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(EventsExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(TaskExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(MemcacheRestExamplesMultiNodeSelfTest.class));
+        suite.addTest(new TestSuite(MonteCarloExamplesMultiNodeSelfTest.class));
+
+        // Binary.
+        suite.addTest(new TestSuite(CacheClientBinaryExampleTest.class));
+        suite.addTest(new TestSuite(ComputeClientBinaryExampleTest.class));
+
+        return suite;
+    }
+}
