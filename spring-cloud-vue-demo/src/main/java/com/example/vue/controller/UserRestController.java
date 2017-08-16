6b7c316021c41c4f3c6eb7c70b1f4942d3940786
heheshang <2448318433@qq.com>
2017-08-16 23:26:24 +0800

    uu

---
+++ spring-cloud-vue-demo/src/main/java/com/example/vue/controller/UserRestController.java
@@ -0,0 +1,104 @@
+package com.example.vue.controller;
+
+
+import com.example.vue.common.RestResult;
+import com.example.vue.common.RestResultGenerator;
+import com.example.vue.common.Utils;
+import com.example.vue.user.IUserService;
+import com.example.vue.user.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Created by kaenry on 2016/9/8.
+ * UserRestController
+ */
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+    @Autowired
+    IUserService userService;
+
+    /**
+     * get all user, GET
+     * @return
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public RestResult<List<User>> all() {
+        List<User> all = userService.findAll();
+        return RestResultGenerator.genSuccessResult(all);
+    }
+
+    /**
+     * add single user
+     * @param user username, password
+     * @return RestResult
+     * @throws Exception valid check
+     */
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public RestResult<User> save(@Valid @RequestBody User user) throws Exception {
+        User save = userService.save(user);
+        return RestResultGenerator.genSuccessResult(save);
+    }
+
+    /**
+     * get single user by id, GET /id
+     * @param id user id
+     * @return RestResult<User>
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public RestResult<User> get(@PathVariable Long id) throws Exception {
+        User user = userService.findById(id);
+        return RestResultGenerator.genSuccessResult(user);
+    }
+
+    /**
+     * delete user by id
+     * @param id user id
+     * @return success
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public RestResult delete(@PathVariable Long id) throws Exception {
+        userService.delete(id);
+        return RestResultGenerator.genSuccessResult();
+    }
+
+    /**
+     * update user for all props
+     * @param id update user id
+     * @param newUser new props
+     * @return updated User
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public RestResult<User> updateAll(@PathVariable Long id, @Valid @RequestBody User newUser) throws Exception {
+        User user = userService.findById(id);
+        // copy all new user props to user except id
+        BeanUtils.copyProperties(newUser, user, "id");
+        user = userService.save(user);
+        return RestResultGenerator.genSuccessResult(user);
+    }
+
+    /**
+     * update user for some props
+     * @param id update user id
+     * @param newUser some props
+     * @return updated user
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+    public RestResult<User> update(@PathVariable Long id, @Valid @RequestBody User newUser) throws Exception {
+        User user = userService.findById(id);
+        // copy all new user props to user except null props
+        BeanUtils.copyProperties(newUser, user, Utils.getNullPropertyNames(newUser));
+        user = userService.save(user);
+        return RestResultGenerator.genSuccessResult(user);
+    }
+}
