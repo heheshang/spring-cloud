4b69b98355622d94a3e17afc51af07348ddc99af
heheshang <2448318433@qq.com>
2017-08-16 17:30:46 +0800

    vue

---
+++ spring-cloud-vue-demo/src/main/java/com/example/vue/VueDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueDemoApplication.class, args);
+	}
+}
